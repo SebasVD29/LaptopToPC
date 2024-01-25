82c90e021cf3b6672bbc2fc8e2a75ee7d376cac6
SebasVD29 <100324444+SebasVD29@users.noreply.github.com>
2024-01-24 20:15:42 -0600

    Semana2Jump

--- Ulatina/IX - Cuatrimestre/Proyecto 4/Semanas/2/Semana2/Assets/Scripts/Player/Player.cs
+++ Ulatina/IX - Cuatrimestre/Proyecto 4/Semanas/2/Semana2/Assets/Scripts/Player/Player.cs
@@ -7,7 +7,14 @@ public class Player : MonoBehaviour
     public float runSpeed = 2;
     public float jumpSpeed = 3;
     Rigidbody2D player;
-    Jump playerJump;
+    SpriteRenderer playerRender;
+    public Jump playerJump;
+    Animator animator;
+
+    public bool betterJump;
+    public float fallMultiplier = 0.5f;
+    public float lowJumpMultiplier = 1;
+        
 
 
 
@@ -15,7 +22,9 @@ public class Player : MonoBehaviour
     void Start()
     {
         player = GetComponent<Rigidbody2D>();
-        playerJump = GetComponent<Jump>();
+        playerRender = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
+        
     }
 
     // Update is called once per frame
@@ -23,22 +32,47 @@ public class Player : MonoBehaviour
     {
         if (Input.GetKey("d") || Input.GetKey("right"))
         {
-           player.velocity = new Vector2 (runSpeed, player.velocity.y);
+
+            player.velocity = new Vector2(runSpeed + Time.deltaTime , player.velocity.y);
+            playerRender.flipX = false;
+            animator.SetFloat("Runing", runSpeed);
         }
         else if (Input.GetKey("a") || Input.GetKey("left"))
         {
-           player.velocity = new Vector2 (-runSpeed, player.velocity.y);
+            player.velocity = new Vector2 (-runSpeed + Time.deltaTime, player.velocity.y);
+            playerRender.flipX = true;
+            animator.SetFloat("Runing", runSpeed);
         }
         else
         {
            player.velocity = new Vector2 (0, player.velocity.y);
+            animator.SetFloat("Runing", 0);
         }
 
         if (Input.GetKey("space") && playerJump.isGrounded)
         {
-            player.velocity = new Vector2(player.velocity.x, jumpSpeed);
+            player.velocity = new Vector2(player.velocity.x, jumpSpeed );
         }
+        if (betterJump)
+        {
+            if (player.velocity.y < 0)
+            {
+                player.velocity += Vector2.up * Physics2D.gravity.y * (fallMultiplier) * Time.deltaTime;
+            }
+            if (player.velocity.y < 0 && !Input.GetKey("space"))
+            {
+                player.velocity += Vector2.up * Physics2D.gravity.y * (lowJumpMultiplier) * Time.deltaTime;
 
+            }
+        }
+        if (playerJump.isGrounded)
+        {
+            animator.SetBool("isJumping", false);
+        }
+        else
+        { 
+            animator.SetBool("isJumping", true);
+        }
 
 
     }
